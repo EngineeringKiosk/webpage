1f6fc104b43edb4e2034a0af907eb641e291f3c1
Wolfgang Gassler <wolfgang@gassler.org>
2023-02-10 21:56:27 +0100

    more tests

--- scripts/test_podcast-feed-to-content.py
+++ scripts/test_podcast-feed-to-content.py
@@ -34,3 +34,18 @@ def test_modify_openpodcast_up_down_voting_check_greedy():
 
     #assert that there are still two starting p tags and the algo wasn't greedy
     assert new_html_content.count("<p") == 2
+
+def test_modify_openpodcast_up_down_voting_dont_touch_other_stuff():
+    html_content = """<h3>
+    <a href="https://otherurl" rel="nofollow"><strong>👍</strong> (sehr cool)</a>
+    </h3>
+    other stuff here
+    <h3>
+    <a href="https://api.openpodcast.dev/feedback/57/downvote" rel="nofollow"><strong>👍</strong> (sehr cool)</a>
+    </h3>"""
+
+    # Modify the HTML content
+    new_html_content = modify_openpodcast_up_down_voting(html_content)
+
+    #assert that there are still two h3 tags
+    assert new_html_content.count("h3>") == 2
