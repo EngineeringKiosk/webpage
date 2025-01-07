0f57d5ed2ba76b4d054560474f41e0039e62e0dd
cstanger <cstanger@google.com>
2025-01-07 15:09:56 +0000

    add meetup qr redirect for future placeholder redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 // do not add to sitemap if specified string is contained in path
-const exludeFromSitemap = ['meetup/alps/promote/'];
+const exludeFromSitemap = ['meetup/alps/promote/', 'meetup/alps/qr/'];
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://engineeringkiosk.dev/',
 	trailingSlash: 'always',
-
+	// redirects: {
+	// 	'/meetup/alps/qr/': 'https://google.at'
+	//   },
 	integrations: [
 		tailwind(),
 		sitemap({
